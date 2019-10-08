3aad5d9bd4c2f17b254b1d4baf4bab5357f290f8
tonyxuantong <xuantong@sas.upenn.edu>
2019-10-08 15:35:07 -0600

    Add files via upload

--- python/step_1_isolatedforest.py
+++ python/step_1_isolatedforest.py
@@ -12,63 +12,52 @@ import numpy as np
 from scipy.stats import chi2
 import seaborn as sns
 
-
-filename = "/Volumes/Extreme 500/GINI/Python/v4/data0922.csv"
+#read all the data points
+filename = "point_data.csv"
 df_0= pd.read_csv(filename,delimiter = ',')
 df_0['NTL'].fillna(0, inplace=True)
+#remove and reclassify poopulation and NTL 
 df_0 = df_0[(df_0['POP'] > 0)]
 df_0.loc[df_0.NTL < 0, 'NTL'] = 0
 
 
-#join with continent and income data
-filename2 = "/Volumes/Extreme 500/GINI/Python/data/income_code.csv"
+#join with income classification
+filename2 = "income_classification.csv"
 df_inc= pd.read_csv(filename2,delimiter = ',')
+df2 = pd.merge(df_0, df_inc,on='GID_0',how = 'left')
+df2['Income'] = df2.Income.fillna('None')
+print(len(df2.GID_0.unique()))
 
-filename3 = "/Volumes/Extreme 500/GINI/Python/data/continentcode.csv"
-df_cont= pd.read_csv(filename3,delimiter = ',')
-
-filename = "/Volumes/Extreme 500/GINI/Python/data/gadm2_info.csv"
-df_name1= pd.read_csv(filename, delimiter = ',',usecols=['GID_0','GID_2'])
-filename = "/Volumes/Extreme 500/GINI/Python/data/gadm2newid.csv"
-df_name2= pd.read_csv(filename, delimiter = ',',usecols=['GID_2','New_ID2'])
-df_name2.columns = ['GID_2','gadm2_1']
-
-df_name_new = pd.merge(df_name1, df_name2,on='GID_2')
-
-#merge newid with continent and income
-df2 = pd.merge(df_0, df_inc,on='GID_0')
+#select urban population that does not have NTL value
 df_urban_other = df2[(df2['POP'] > 0) & (df2['SMOD'] > 20)&(df2['NTL'] == 0)]
+
+#select rural population
 df_rural = df2[(df2['POP'] > 0) & (df2['SMOD'] <20)]
+
+#selecrt urban population with NTL >0
 df2 = df2[(df2['POP'] > 0) & (df2['SMOD'] > 20) &(df2['NTL'] > 0)]
-secondqt = np.percentile(df2.NTL, 50)
-df2_p1 =  df2[(df2['NTL'] > secondqt)]
-df2_p2 =  df2[(df2['NTL'] <= secondqt)]
+
+#set a threshold for selecting data for iForest test based on medium NTL value
+thld = np.percentile(df2.NTL, 50)
+
+#data for anomaly detection
+df2_p1 =  df2[(df2['NTL'] > thld)]
+
+#data considered as normal
+df2_p2 =  df2[(df2['NTL'] <= thld)]
 df2 = df2_p1
-#4 sets of data: df_p2, df2, df_urban_other, df_rural
 
 
 #filter data
+#save a copy
 popAndNtl= df2
-
+#rename the columns
 popAndNtl_v2 = df2[["POP", "NTL"]]
-#popAndNtl.plot.scatter(x = "POP", y = "NTL")
-#test1 = popAndNtl[popAndNtl['NTL']<1]
 
-
+#import iforest
 from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-
-#clf = IsolationForest(max_samples=10000, contamination=.01)
-#clf.fit(popAndNtl)
-#outliers_predicted = clf.predict(popAndNtl)
-##check the results
-#df2['outlier'] = outliers_predicted
-#plt.figure(figsize = (10,5))
-#plt.scatter(df2['POP'], df2['NTL'], c=df2['outlier'])
-#plt.show()
-
-
 
 
 list_inc = popAndNtl.Income.unique().tolist()
@@ -89,16 +78,15 @@ for inc_i in list_inc:
         scaler = MinMaxScaler()
         new_df = scaler.fit_transform(new_df)
         new_df = pd.DataFrame(new_df, columns = ["POP", "NTL"])
-        #new_df.plot.scatter(x = "POP", y = "NTL")
-        ##test2 = popAndNtl.sort_values('NTL')
+
         #detect outliers
         clf = IsolationForest(max_samples=5000, contamination=.001)
         clf.fit(new_df)
         outliers_predicted = clf.predict(new_df)
+
         #append cluster outlier
         popAndNtl_temp['Outlier'] = outliers_predicted
         #append to dataframe
-        
         #all all data
         df_p2_v2 = df2_p2[(df2_p2['Income']==inc_i)]
         df_rest = df_rest.append(df_p2_v2) 
@@ -111,34 +99,26 @@ for inc_i in list_inc:
        
         dfall = dfall.append(df_rest) 
 
-        #plot
-        plt.figure(figsize = (30,15))
-        plt.scatter(df_rest['POP'], df_rest['NTL'], c=df_rest['Outlier'])
-        #plt.show()
-        title = str(inc_i)+'.png'
-        plt.savefig(title)
-
-plt.figure(figsize = (10,5))
-plt.scatter(dfall['POP'], dfall['NTL'], c=dfall['Outlier'])
-plt.savefig('final.png')
-#plt.show()
-
-
 
 import statistics
 print(len(dfall.loc[dfall.Outlier == -1]))
 de_outlier = dfall.loc[dfall.Outlier == -1]
-dfall.to_csv('outlier.csv',index = False)
 
+#save all the outliers
+de_outlier.to_csv('outlier.csv',index = False)
 
+#reclassify outliers to 0
 ntl_mean = int(statistics.mean(dfall.NTL.tolist()))
 dfall.loc[dfall.Outlier == -1, 'NTL'] = 0
+
+#drop ourlier column
 dfall.drop('Outlier', axis=1, inplace=True)
 
+#save final data to file
 df_urban_final= dfall[(dfall['SMOD'] > 20)]
-df_urban_final.to_csv('if_urban_filtered.csv',index = False)
 
-dfall.to_csv('all_data.csv',index = False)
+#save all the urban points
+df_urban_final.to_csv('urban_points.csv',index = False)
 
-#popAndNtl['outlier'] = clusters
-#outlier_count = np.count_nonzero(clusters == -1)
+#save all points
+dfall.to_csv('all_points.csv',index = False)
